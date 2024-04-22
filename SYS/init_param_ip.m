52fd658a345ba282beccc731ac748a76d0682c3c
Mandresy <randrianarijaonamandresy@gmail.com>
2024-04-22 14:43:56 +0200

    Removed unecessart files from cache

---
+++ SYS/init_param_ip.m
@@ -0,0 +1,10 @@
+kt = 1.37; %[Nm/A]
+V_supplied = 12; %[V]
+r = 0.036; % [m]
+R_motor = 10.7; % [Ohm] Resistance of the Armature of the DC Motor
+%(encoderCount / encoderCountsPerUnit) * distancePerCount * 3.14
+encoderCountsPerUnit = 1040; %Encoder counts per unit of distance
+distancePerCount = 0.036; %Distance covered per encoder count [m]
+m = 0.102; %mass of the pendulum [kg]
+
+
